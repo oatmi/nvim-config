18ef0203e442542c8a311bb324c45d23cbc1ed26
oatmi <gnayoat@gmail.com>
2023-01-06 16:53:13 +0800

    change to leader f

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,9 +3,9 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 
-vim.keymap.set('n', '<leader>f', function() 
+vim.keymap.set('n', '<leader>a', function() 
     return builtin.grep_string({ search = vim.fn.expand("<cword>") })
 end, {})
 

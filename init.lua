3d51544e6cbd85c350e570873b80767a350ea322
杨涛 <yangtao@yangtaos-Mac-mini.local>
2023-02-04 22:26:37 +0800

    update idbg cmd

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ require("tao")
 -- https://github.com/ellisonleao/gruvbox.nvim
 vim.o.background = "dark" -- or "light" for light mode
 
-vim.cmd('abb idbg fmt.Printf(\"debug: %+v\n\",')
+vim.cmd('abb idbg fmt.Printf(\"debug: %+v\\n\",')
 vim.cmd('abb iuid 3779255524')

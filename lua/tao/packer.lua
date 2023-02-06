e9867f2be62df7f94612b16e8f0fccae18ff53db
oatmi <gnayoat@gmail.com>
2023-02-06 17:27:23 +0800

    change color scheme to gruvbox

--- lua/tao/packer.lua
+++ lua/tao/packer.lua
@@ -13,11 +13,19 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
+    -- use({
+    --     'rose-pine/neovim',
+    --     as = 'rose-pine',
+    --     config = function()
+    --         vim.cmd('colorscheme rose-pine')
+    --     end
+    -- })
+
     use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
+        'morhetz/gruvbox',
+        as = 'gruvbox',
         config = function()
-            vim.cmd('colorscheme rose-pine')
+            vim.cmd('colorscheme gruvbox')
         end
     })
 

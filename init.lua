e515707a27095462a4ad6afed92f6781af582d1a
Troy <gnayoat@gmail.com>
2023-02-08 15:43:45 +0800

    add icmt and itime abb

--- init.lua
+++ init.lua
@@ -5,3 +5,8 @@ vim.o.background = "dark" -- or "light" for light mode
 
 vim.cmd('abb idbg fmt.Printf(\"debug: %+v\\n\",')
 vim.cmd('abb iuid 3779255524')
+vim.cmd([[inoreabbrev itime <c-r>=strftime("%Y/%m/%d %H:%M:%S")<CR>]])
+vim.cmd([[inoreabbrev icmt // Copyright(C) 2023 Baidu Inc. All Rights Reserved.<CR>
+            \//<CR>
+            \// Author  tao (yangtao23@baidu.com)<CR>
+            \// Date    <c-r>=strftime("%Y/%m/%d %H:%M:%S")<CR>]])

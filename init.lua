8508c139908d5386fc0a0133086d65de54dff5b0
Troy <gnayoat@gmail.com>
2023-05-05 10:34:58 +0800

    bg none

--- init.lua
+++ init.lua
@@ -11,8 +11,7 @@ vim.cmd([[inoreabbrev icmt // Copyright(C) 2023 Baidu Inc. All Rights Reserved.<
             \// Author  tao (yangtao23@baidu.com)<CR>
             \// Date    <c-r>=strftime("%Y/%m/%d %H:%M:%S")<CR>]])
 
-vim.cmd[[highlight NonText guibg=none]]
-vim.cmd[[highlight Normal guibg=none]]
+vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
 
 -- git diff tool
 -- https://github.com/banga/git-split-diffs

75eef151bcb8d3cb22cea13720a2e7b3aa5fae41
Troy <gnayoat@gmail.com>
2023-05-18 11:27:24 +0800

    buffer line

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,11 +1,10 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{
-     options = {
-            mode = "tabs",
-            hover = {
-                enabled = true,
-                delay = 200,
-                reveal = {'close'}
-            }
-        }
+    options = {
+        mode = "tabs",
+        numbers = "ordinal",
+        get_element_icon = function()
+            return "",""
+        end,
+    }
 }

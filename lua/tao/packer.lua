b022a6237fe504b5498e473857d98eaf2cb936b5
Troy <gnayoat@gmail.com>
2023-05-24 19:17:38 +0800

    add lualine theme

--- lua/tao/packer.lua
+++ lua/tao/packer.lua
@@ -79,4 +79,10 @@ return require('packer').startup(function(use)
             'nvim-tree/nvim-web-devicons'
         }
     }
+
+    -- use { "catppuccin/nvim", as = "catppuccin" }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)

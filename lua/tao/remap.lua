8054d4902a251ff2ae50b360aaa88468fcc2de4d
Troy <gnayoat@gmail.com>
2023-05-24 14:16:57 +0800

    add nmap to gt and gT

--- lua/tao/remap.lua
+++ lua/tao/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 vim.keymap.set("n", "<leader>w", ':w<CR>')
 vim.keymap.set("n", "<leader>q", ':q<CR>')
 
@@ -7,3 +7,5 @@ vim.keymap.set('n', '<leader>h', '<C-w>h')
 vim.keymap.set('n', '<leader>j', '<C-w>j')
 vim.keymap.set('n', '<leader>k', '<C-w>k')
 vim.keymap.set('n', '<leader>l', '<C-w>l')
+vim.keymap.set('n', '<leader>n', 'gt')
+vim.keymap.set('n', '<leader>N', 'gT')

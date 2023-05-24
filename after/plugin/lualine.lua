a0ba3598da691f3730ad0d1a2769f1781d4fea0d
Troy <gnayoat@gmail.com>
2023-05-24 19:49:38 +0800

    add lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,2 +1,55 @@
 require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    -- component_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'tabs', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {
+        {
+            'datetime',
+            -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+            style = "%m/%d %H:%M"
+        }
+    }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
+
+-- https://github.com/nvim-lualine/lualine.nvim#custom-components
+--
+-- local function hello()
+--   return [[hello world]]
+-- end
+-- sections = { lualine_a = { hello } }

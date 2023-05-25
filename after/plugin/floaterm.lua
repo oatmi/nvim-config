2f20e0ae17ef49974af51c8ae2641e4355f4f600
Troy <gnayoat@gmail.com>
2023-05-25 11:30:44 +0800

    floaterm guibg to black

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,1 +1,9 @@
+-- https://github.com/voldikss/vim-floaterm
+
+-- Set floaterm window's background to black
+vim.cmd([[hi Floaterm guibg=black]])
+
+-- Set floating window border line color to cyan, and background to orange
+-- hi FloatermBorder guibg=orange guifg=cyan
+
 vim.keymap.set('n', '<C-n>', vim.cmd.FloatermToggle)

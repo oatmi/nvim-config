5fcbeb79b75bbc55cd4c26a5d130a77d0b90f6d5
Troy <gnayoat@gmail.com>
2023-05-25 19:14:23 +0800

    change telescope layout

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,23 @@
 
 local builtin = require('telescope.builtin')
 
+require('telescope').setup({
+    defaults = {
+        layout_strategy = 'vertical',
+        layout_config = {
+            vertical = {
+                width = 0.8,
+                height = 0.8,
+                prompt_position = "bottom"
+            }
+            -- other layout configuration here
+        },
+        -- other defaults configuration here
+    },
+    -- other configuration values here
+})
+
+
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 
@@ -9,7 +26,7 @@ vim.keymap.set('n', '<leader>a', function()
     return builtin.grep_string({ search = vim.fn.expand("<cword>") })
 end, {})
 
- -- vim.keymap.set('n', '<leader>f', builtin.grep_string({search = "hi"}), { search = vim.fn.expand("<cword>") })
+-- vim.keymap.set('n', '<leader>f', builtin.grep_string({search = "hi"}), { search = vim.fn.expand("<cword>") })
 
 -- local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})

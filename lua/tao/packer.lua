694184fd667f223d75d765569453ff0ff0ff3f3b
Troy <gnayoat@gmail.com>
2023-06-07 15:14:40 +0800

    update treesitter config

--- lua/tao/packer.lua
+++ lua/tao/packer.lua
@@ -80,9 +80,9 @@ return require('packer').startup(function(use)
         }
     }
 
-    -- use { "catppuccin/nvim", as = "catppuccin" }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+    use { "catppuccin/nvim", as = "catppuccin" }
+   use {
+       'nvim-lualine/lualine.nvim',
+       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+   }
 end)

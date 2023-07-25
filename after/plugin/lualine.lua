a91cb1666368c9d55993b90f65b060924f52929a
Troy <gnayoat@gmail.com>
2023-07-25 10:20:50 +0800

    lua

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,15 +1,13 @@
 require('lualine').setup {
   options = {
-
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
-
     icons_enabled = false,
     theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     -- component_separators = { left = '', right = ''},
     -- section_separators = { left = '', right = ''},
-    -- component_separators = { left = '', right = ''},
-    -- section_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = '' },
+    -- component_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},

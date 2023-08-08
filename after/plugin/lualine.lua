8dc0d4922e617b31d751f793af7c267768e2114a
Troy <gnayoat@gmail.com>
2023-08-08 10:43:09 +0800

    lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,52 +1,55 @@
 require('lualine').setup {
-  options = {
-    icons_enabled = false,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    -- component_separators = { left = '', right = ''},
-    -- section_separators = { left = '', right = ''},
-    -- section_separators = { left = '', right = '' },
-    -- component_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'diff', 'diagnostics'},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {'branch'},
-    lualine_z = {
-        {
-            'datetime',
-            -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-            style = "%m/%d %H:%M"
+    options = {
+        icons_enabled = false,
+        show_filename_only = false,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = '' },
+        -- component_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
         }
-    }
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+    },
+
+    -- https://github.com/nvim-lualine/lualine.nvim
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'diff', 'diagnostics'},
+        lualine_c = {'branch', { 'filename', path = 1 }, 'progress'},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+            {
+                'datetime',
+                -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+                style = "%m/%d %H:%M"
+            }
+        }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
 
 -- https://github.com/nvim-lualine/lualine.nvim#custom-components

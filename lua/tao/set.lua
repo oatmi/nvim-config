53178c2df9a8d564a7f8031db6af7f127b11dfcc
Troy <gnayoat@gmail.com>
2023-08-14 12:11:11 +0800

    float term

--- lua/tao/set.lua
+++ lua/tao/set.lua
@@ -22,9 +22,9 @@ vim.opt.filetype    = "on"
 vim.opt.clipboard   = "unnamedplus"
 
 -- float_term
-vim.g.floaterm_width  = 0.8
-vim.g.floaterm_height = 0.8
-vim.g.floaterm_borderchars = '─│─│╭╮╯╰'
+vim.g.floaterm_width        = 0.7
+vim.g.floaterm_height       = 0.8
+vim.g.floaterm_borderchars  = '─│─│╭╮╯╰'
 
 
 -- https://github.com/folke/tokyonight.nvim

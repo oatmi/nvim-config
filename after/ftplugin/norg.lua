8e79c90e059567f37cf2e852b6a060607e9330ba
Troy <gnayoat@gmail.com>
2023-09-17 17:52:11 +0800

    s

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,1 +1,6 @@
-vim.opt.nu           = false
+vim.opt.nu             = false
+vim.wo.wrap            = true
+vim.opt.breakindent    = true
+vim.opt.breakindentopt = 'shift:0,min:40,sbr'
+vim.opt.linebreak      = false
+vim.opt.showbreak      = '>'

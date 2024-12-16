aa5c74e3bd29727e345d26f88e1e586a74648132
Troy <gnayoat@gmail.com>
2024-12-16 23:32:24 +0800

    asd

--- init.lua
+++ init.lua
@@ -239,3 +239,4 @@ vim.api.nvim_create_autocmd(
 
 vim.keymap.set({'n', 'x', 'o'}, 'f',  '<Plug>(leap-forward-to)')
 vim.keymap.set({'n', 'x', 'o'}, 'F',  '<Plug>(leap-backward-to)')
+vim.keymap.set({'n', 'x', 'o'}, 'gf', '<Plug>(leap-from-window)')
